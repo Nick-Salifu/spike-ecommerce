41cbbe2ff5780ebc22aef8777064c562ca0ebd63
Salifu Friday Nicodemus <nicksalifu@gmail.com>
2025-04-07 23:37:19 +0100

    working on the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
+import { Button } from "@/components/ui/button";
 import { stripe } from "@/lib/stripe";
+import Image from "next/image";
+import Link from "next/link";
 
 export default async function Home() {
   const products = await stripe.products.list({
@@ -7,6 +10,19 @@ export default async function Home() {
   })
 
   return (
-    <div>Home page</div>
+    <div>
+      <section>
+        <div>
+          <div>
+            <h2>Welcome to spike</h2>
+            <p>Discover the best and latest products at affordable prices.</p>
+            <Button asChild variant="default">
+              <Link href="/products">Browse All Products</Link>
+            </Button>
+          </div>
+          <Image alt="Banner Image" width={450} height={450} src={products.data[0].images[0]} />
+        </div>
+      </section>
+    </div>
   );
 }
