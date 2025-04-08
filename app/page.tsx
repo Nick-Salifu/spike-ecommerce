3ec3c55ce6a9e1093cc3eb2debb5108d74f07188
Salifu Friday Nicodemus <nicksalifu@gmail.com>
2025-04-08 23:44:27 +0100

    calling items from stripe

--- app/page.tsx
+++ app/page.tsx
@@ -11,13 +11,15 @@ export default async function Home() {
 
   return (
     <div>
-      <section>
-        <div>
-          <div>
-            <h2>Welcome to spike</h2>
-            <p>Discover the best and latest products at affordable prices.</p>
-            <Button asChild variant="default">
-              <Link href="/products">Browse All Products</Link>
+      <section className="rounded bg-neutral-100 py-8 sm:py-12">
+        <div className="mx-auto grid grid-cols-1 items-center justify-items-center gap-8 px-8 sm:px-16">
+          <div className="max-w-md space-y-4">
+            <h2 className="text-3xl font-bold tracking-tight md:text-4xl">
+              Welcome to spike
+            </h2>
+            <p className="text-neutral-600">Discover the best and latest products at affordable prices.</p>
+            <Button asChild variant="default" className="inline-flex items-center rounded-full px-6 py-3 bg-black text-white">
+              <Link href="/products" className="inline-flex items-center justify-center rounded-full px-6 py-3">Browse All Products</Link>
             </Button>
           </div>
           <Image alt="Banner Image" width={450} height={450} src={products.data[0].images[0]} />
