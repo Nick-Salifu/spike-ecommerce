655c72ffe2d818ce33943030e08b3b0f852cc184
Salifu Friday Nicodemus <nicksalifu@gmail.com>
2025-04-11 23:57:09 +0100

    creating the checkout page

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,5 +1,44 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {useCartStore} from "@/store/cart-store"
+
 export default function CheckoutPage() {
+    const {items, removeItem, addItem } = useCartStore()
+    const total = items.reduce((acc, item) => acc + item.price * item.quantity, 0);
+
+    if (total === 0 || items.length === 0) {
+        return (
+            <div>
+                <h1>Your cart is empty</h1>
+            </div>
+        )
+    }
+
     return (
-        <div>Checkout Page</div>
+        <div>
+            <h1>Checkout</h1>
+            <Card>
+                <CardHeader>
+                    <CardTitle>Order Summary</CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <ul>
+                        {items.map((item, key) => (
+                            <li key={key}>
+                                <div>
+                                    <span>{item.name}</span>
+                                    <span>${((item.price * item.quantity) / 100).toFixed(2)}</span>
+                                </div>
+                                <div>
+                                    <Button variant="outline" onClick={() => removeItem(item.id)}>-</Button>
+                                        <span className="text-lg font-semibold">{item.quantity}</span>
+                                    <Button onClick={() => addItem(item)}>+</Button>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </CardContent>
+            </Card>
+        </div>
     )
 }
