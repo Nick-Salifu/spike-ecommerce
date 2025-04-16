7a7784e31c0e397ba2341cc9d057eb19711dc250
Salifu Friday Nicodemus <nicksalifu@gmail.com>
2025-04-16 14:21:56 +0100

    error fix

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -4,15 +4,13 @@ import { stripe } from "@/lib/stripe";
 export default async function ProductPage({
   params,
 }: {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }) {
-  const { id } = params;
-
+  const { id } = await params;
   const product = await stripe.products.retrieve(id, {
     expand: ["default_price"],
   });
 
   const plainProduct = JSON.parse(JSON.stringify(product));
-
   return <ProductDetails product={plainProduct} />;
 }
