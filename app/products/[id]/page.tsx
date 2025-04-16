623d58861b6ab61699915172ab0cba3175e535bc
Salifu Friday Nicodemus <nicksalifu@gmail.com>
2025-04-16 13:52:28 +0100

    fix

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -2,9 +2,8 @@ import { ProductDetails } from "@/components/product-details"
 import { stripe } from "@/lib/stripe"
 
 
-export default async function ProductPage({ params }: { params: Promise<{ id: string }>; }) {
-    const { id } = await params;
-    const product = await stripe.products.retrieve(id, {
+export default async function ProductPage({params}: {params: {id: string}}) {
+    const product = await stripe.products.retrieve(params.id, {
         expand: ["default_price"]
     })
 
